312a08a7089458bbcaefb5c3088e7e4aa2fd4fac
chxry <chxry@floppa.world>
2024-01-18 23:01:10 +0000

    version 4?

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    fontFamily: {
+      mono: ["JetBrains Mono", "monospace"],
+    },
+  },
+  plugins: [require("@catppuccin/tailwindcss")],
+};
